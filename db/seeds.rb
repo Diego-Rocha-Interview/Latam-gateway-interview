050b7fb620b18e3e11801e42a7982b70ceab5df1
Diego-Rocha-Interview <87577047+Diego-Rocha-Interview@users.noreply.github.com>
2021-07-17 13:06:24 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 Address.delete_all
 
-5.times do |i|
-	Address.create cep: "BAA #{i}"
-end
+Address.create! (cep: "12226620", token:"A1B1C1D1F1G1", email:"test@test.com", password:"test123")
+Address.create! (cep: "12226630", token:"A2B2C2D2F2G2", email:"test2@test.com", password:"test123")
+Address.create! (cep: "18220970", token:"A3B3C3D3F3G3", email:"test3@test.com", password:"test123")
+Address.create! (cep: "54505645", token:"A4B4C4D4F4G4", email:"test4@test.com", password:"test123")
+Address.create! (cep: "80030460", token:"A5B5C5D5F5G5", email:"test5@test.com", password:"test123")
+Address.create! (cep: "58055770", token:"A6B6C6D6F6G6", email:"test6@test.com", password:"test123")
+Address.create! (cep: "72006055", token:"A7B7C7D7F7G7", email:"test7@test.com", password:"test123")
+Address.create! (cep: "74923430", token:"A8B8C8D8F8G8", email:"test8@test.com", password:"test123")
